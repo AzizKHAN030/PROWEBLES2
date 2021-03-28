8772eb95e060f6837180ce6ebef8f4a219a2ea7f
Azizbek <arasulov0066@gmail.com>
2021-03-28 20:25:57 +0500

    done all

--- script.js
+++ script.js
@@ -1,26 +1,47 @@
-let answ=document.getElementById(`answ`);
+let answ = document.getElementById(`answ`);
 let btn = document.getElementById(`btn`);
-let result= document.getElementById(`result`);
-let rand=Math.round(Math.random()*100);
+let result = document.getElementById(`result`);
+let count = 10;
+
+function random() {
+    return Math.round(Math.random() * 100);
+};
+
+rand = random();
+
 console.log(rand);
-btn.onclick=function(){
-    if(!isNaN(answ.value)){
-   
-        if(Math.abs(rand-answ.value)>=1 && Math.abs(rand-answ.value)<=10){
-            result.innerHTML = `Горячо`;
+
+btn.onclick = function () {
+    if (!isNaN(answ.value)) {
+        if (count > 1) {
+            if (Math.abs(rand - answ.value) >= 1 && Math.abs(rand - answ.value) <= 10) {
+                count--;
+                result.innerHTML = `Горячо, осталось попыток ${count}`;
+            } else if (Math.abs(rand - answ.value) >= 11 && Math.abs(rand - answ.value) <= 20) {
+                count--;
+                result.innerHTML = `Тепло, осталось попыток ${count}`;
+            } else if (Math.abs(rand - answ.value) >= 21) {
+                count--;
+                result.innerHTML = `Холодно, осталось попыток ${count}`;
+            } else if (Math.abs(rand - answ.value) == 0) {
+                if (confirm(`В точку! \n Хотите попробовать еще раз?`)) {
+                    count = 10;
+                    result.innerHTML = `У вас еще ${count} попыток!`;
+                    rand = random();
+                    console.log(rand);
+                } else {
+                    alert(`Всего хорошего!`);
+                    btn.disabled = true;
+                }
+            }
+        } else {
+            if (confirm(`Вы не угадали! \n Хотите попробовать еще раз?`)) {
+                count = 10;
+                result.innerHTML = `У вас еще ${count} попыток!`;
+                btn.disabled = false;
+            } else(alert(`Может быть в другой раз?`));
         }
-       else if(Math.abs(rand-answ.value)>=11 && Math.abs(rand-answ.value)<=20){
-            result.innerHTML = `Тепло`;
-        }
-       else if(Math.abs(rand-answ.value)>=21){
-            result.innerHTML = `Холодно`;
-        } 
-        else if(Math.abs(rand-answ.value)==0){
-            result.innerHTML = `В точку!`;           
-        }
-        
-}
-else{
-    result.innerHTML = `Введите число!`;
-}
+    } else {
+        result.innerHTML = `Введите число!`;
+    }
 }
